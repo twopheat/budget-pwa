2d233e092200eccd0c8e2f55f26b6dbf6f024d05
twopheat <twopheat@gmail.com>
2020-03-22 19:25:40 -0700

    SUCCESS, now a PWA

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,19 @@
 const FILES_TO_CACHE = [
-    "/offline.html",
-    "/index.html",
-    "/assets/css/styles.css",
-    "/assets/js/app.js",
-    "/assets/js/budget.js",
-    "/assets/js/install.js",
-    "/assets/images/icon-192x192.png",
-    "/assets/images/icon-512x512.png"
+    "./offline.html",
+    "./index.html",
+    "./assets/css/styles.css",
+    "./assets/js/app.js",
+    "./assets/js/budget.js",
+    "./assets/js/install.js",
+    "./assets/images/icon-192x192.png",
+    "./assets/images/icon-512x512.png"
   ];
   
   const CACHE_NAME = "static-cache-v2";
   const DATA_CACHE_NAME = "data-cache-v1";
   
+
+
   // install
   self.addEventListener("install", function(evt) {
     evt.waitUntil(
